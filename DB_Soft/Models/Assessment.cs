204bf766fb9b33d6bb568da52de00dd5172057cd
Christoffer-Nielsen-Cph <cph-cn332@cphbusiness.dk>
2024-03-02 14:31:55 +0100

    assessment model

---
+++ DB_Soft/Models/Assessment.cs
@@ -0,0 +1,18 @@
+﻿namespace DB_Soft.Models
+{
+    public class Assessment
+    {
+        public int AssessmentID { get; set; }
+        public int AccountNo { get; set; }
+        public string Questionnaire { get; set; }
+        public string AccessType { get; set; }
+        public string AssessmentAttachment { get; set; }
+        public string BoundaryDescription { get; set; }
+        public int PublicationYear { get; set; }
+        public string FactorsConsidered { get; set; }
+        public string PrimaryAuthors { get; set; }
+        public string AdaptationGoals { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+    }
+}
