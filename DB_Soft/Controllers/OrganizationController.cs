853a134c2f5f36be0fe07c75d46f59568ec0b0a0
Christoffer-Nielsen-Cph <cph-cn332@cphbusiness.dk>
2024-03-04 14:52:25 +0100

    All methods added

--- DB_Soft/Controllers/OrganizationController.cs
+++ DB_Soft/Controllers/OrganizationController.cs
@@ -75,6 +75,218 @@ namespace DB_Soft.Controllers
 
             return new JsonResult(table);
         }
+
+        [HttpGet("OrgGases")]
+        public JsonResult GetGasesUsedByOrg(string gas)
+        {
+            if (string.IsNullOrWhiteSpace(gas))
+            {
+                return new JsonResult(new { error = "Gas name is required." }) { StatusCode = 400 }; // Bad Request
+            }
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand("SP_OrgSpecificGas", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@gas", gas);
+       
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        table.Load(sqlReader);
+                    }
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("PopulationDetailsYear")]
+        public JsonResult GetPopulationDetailsByYear(string year)
+        {
+            if (string.IsNullOrWhiteSpace(year))
+            {
+                return new JsonResult(new { error = "Year is required." }) { StatusCode = 400 }; // Bad Request
+            }
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand("SP_PopulationDetailsYear", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@year", year);
+
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        table.Load(sqlReader);
+                    }
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("C40")]
+        public JsonResult GetOrganizationsC40()
+        {
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand("SP_OrganizationC40", myConn))
+                {
+                    myCommand.CommandType = CommandType.StoredProcedure; // If you are using a stored procedure
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+                myConn.Close();
+            }
+
+            return new JsonResult(table);
+        }
+        [HttpGet("Gases")]
+        public JsonResult GetGases(string organizationName)
+        {
+            if (string.IsNullOrWhiteSpace(organizationName))
+            {
+                return new JsonResult(new { error = "Organization name is required." }) { StatusCode = 400 }; // Bad Request
+            }
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand("SP_OrganizationGases", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@organizationName", organizationName);
+
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        table.Load(sqlReader);
+                    }
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("Method")]
+        public JsonResult GetOrganizationMethodologies(string organizationName)
+        {
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("SP_GetOrganizationMethodologies", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@organizationName", organizationName);
+
+                    sqlConnection.Open();
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        table.Load(sqlReader);
+                    }
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+        [HttpGet("CountriesLandAreaAndTemp")]
+        public JsonResult GetCountriesLandAreaAndTemperature()
+        {
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand("SP_GetCountriesLandAreaAndTemperature", myConn))
+                {
+                    myCommand.CommandType = CommandType.StoredProcedure; // If you are using a stored procedure
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+                myConn.Close();
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("OrgGDP")]
+        public JsonResult GetOrganizationsWithGdp(string reportingYear)
+        {
+            if (string.IsNullOrWhiteSpace(reportingYear))
+            {
+                return new JsonResult(new { error = "reporting year is required." }) { StatusCode = 400 }; // Bad Request
+            }
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand("SP_OrganizationWithGDP", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@year", reportingYear);
+
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        table.Load(sqlReader);
+                    }
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("OrganizationEmissions")]
+        public JsonResult GetOrganizationsWithEmissions()
+        {
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand("SP_OrganizationsWithEmissions", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        table.Load(sqlReader);
+                    }
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
     }
 }
 
